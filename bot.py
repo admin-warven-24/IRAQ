15dae370e8fc0171ade64845f120886b5de69bae
admin-warven-24 <145594156+admin-warven-24@users.noreply.github.com>
2026-07-08 11:21:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from telegram import Bot
 BOT_TOKEN = "8941847443:AAF78KW48MF93ntjK8ROGanD1TiEzc4O1_Q"
 SOURCE_CHANNEL = "@hamody_up4"
 TARGET_CHANNEL = "@DUHOK_CC"
-INTERVAL_SECONDS = 3  # هەر ٣ چرکە جارێک پەیامێک دەخوێنێتەوە
+INTERVAL_SECONDS = 3
 # ==============================================
 
 async def copy_and_send(bot, source, target):
@@ -14,7 +14,6 @@ async def copy_and_send(bot, source, target):
         updates = await bot.get_updates()
         if updates and updates[-1].channel_post:
             last_post = updates[-1].channel_post
-            # کۆپی کردنی دەقی پەیامەکە و ناردنی بۆ کەناڵی خۆت
             await bot.send_message(chat_id=target, text=last_post.text)
             print(f"✅ Copied and sent to {target}!")
     except Exception as e:
