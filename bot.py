8a691b9235f1c802c0876336602e7e0b63075379
admin-warven-24 <145594156+admin-warven-24@users.noreply.github.com>
2026-07-20 04:24:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 
+# ========== ڕێکخستنەکان ==========
 api_id = 33774652
 api_hash = "c438941d8f43a0ff59fcc4b3f3c2fb42"
-session = "1AZWarzgBuwesKhPGe92ETUTVwJDmSyA0TupvFCVgJCXy15CjV1MJp5jL3heCfSqobh2__blMqKan0IzeH-5d8oDDK16EiWUS2TScg5FPHJ4SG1eiQMGNzwDW7NMgZWuy4fyICDeB0-iXep3IjFVPQ2Xi78Dk0y26D6oCpiYqUKYWAnbbNXIGFoYq3dskRY1MYWWQI32oKDYkHХ0Нр3МVacmzGqrsPErLRHITWMcGYkgSjNpSzY_sQjuq_tSdqTYzViQOZWSRBFeihtpIz5ny1d4tj8nG6XxTQs81fMguhbTUuRF_t5-TOKRD9vYrs_gm2t1tGI5nwnSuE7HFvg6-XctTmSLKC4A="
+session = "1AZWarzgBuyFQd57gW4J-XbkOy6fp4NDaIshg8b21ufEj5d2iDV9QV2Kgb6XWIJ9PNAOB4ZWLbz36aW-FDKnrmq_fAtICRuj_51HhvLs4NvzuTk3VpZBz5K0vP9ark2797VaktricuGKGD5HU7Gn8Hn30YTd6E-DwUt7tfw2SqVJ_P6AdVab81Sxw5yLytxZhbcntdVLNRVc-WofkSWwj_UQyQkPIKK1GlFaLqRbbt45UaRho7s63BT4iukyNfywBdyRPrbWEelLHAdcdzRtFXiLzO6iGzd4EDX3av9oy2u7VO0JbbQ1CKJfCHQ4-8vbMfifE3SNryrzVtX1GNzw_TVZPVnW5NI8="
 
 SOURCE_CHANNEL = "@WarnisxCcScrap"
 TARGET_CHANNEL = "@kurdiraq7272"
+# ===================================
 
 client = TelegramClient(StringSession(session), api_id, api_hash)
 
@@ -19,9 +21,18 @@ async def handler(event):
 
     if msg.media:
         data = await msg.download_media(file=bytes)
-        await client.send_file(TARGET_CHANNEL, data, caption=new_text, formatting_entities=msg.entities)
+        await client.send_file(
+            TARGET_CHANNEL,
+            data,
+            caption=new_text,
+            formatting_entities=msg.entities
+        )
     else:
-        await client.send_message(TARGET_CHANNEL, new_text, formatting_entities=msg.entities)
+        await client.send_message(
+            TARGET_CHANNEL,
+            new_text,
+            formatting_entities=msg.entities
+        )
 
 print("Bot is running...")
 client.start()
